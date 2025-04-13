c57dcfbe82ef3589dd41ec7ce6292a23a5d89038
Giovanni Henrique <giovannihpf@gmail.com>
2025-04-13 15:47:56 -0300

    Começando a fazer o design da página de informações gerais.

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
-import { Main } from './styled';
+import { Main, Qualifications, Section } from './styled';
 export function Home() {
   return (
     <Main>
       <h1>Informações Gerais</h1>
+      <Section>
+        <h1>Giovanni Henrique de Paula Fernandes</h1>
+        <section>
+          <h2>Tecnologias que mais uso:</h2>
+          <Qualifications>
+            <li>
+              <img
+                src="https://img.shields.io/badge/-JavaScript-0D1117?style=for-the-badge&logo=javascript&labelColor=0D111"
+                alt="JavaScript Icon"
+              />
+            </li>
+
+            <li>
+              <img
+                src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"
+                alt="typescript icon"
+              />
+            </li>
+
+            <li>
+              <img
+                src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"
+                alt="React Icon"
+              />
+            </li>
+
+            <li>
+              <img
+                src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
+                alt="HTML5 icon"
+              />
+            </li>
+
+            <li>
+              <img
+                src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
+                alt="css3 icon"
+              />
+            </li>
+
+            <li>
+              <img
+                src="https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white"
+                alt="Github Icon"
+              />
+            </li>
+          </Qualifications>
+        </section>
+      </Section>
     </Main>
   );
 }
--- src/components/Home/styled.tsx
+++ src/components/Home/styled.tsx
@@ -1,3 +1,70 @@
 import styled from 'styled-components';
+import * as colors from '../../configs/colors';
 
-export const Main = styled.main``;
+export const Main = styled.main`
+  margin: 0 auto;
+  width: 150rem;
+  min-height: 90vh;
+  height: 100%;
+  h1 {
+    margin: 20px 0;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    font-size: 40px;
+    color: ${colors.primaryColor};
+  }
+`;
+
+export const Section = styled.section`
+  margin-top: 10px;
+  background-color: ${colors.primaryColor};
+  color: ${colors.tertiaryColor};
+  border-radius: 8px;
+  max-width: 100vw;
+  min-width: 60vw;
+  width: 95%;
+  margin: 0 auto;
+  min-height: 200px;
+  padding: 10px;
+  h1 {
+    font-size: 3rem;
+    margin-top: 10px;
+    color: ${colors.emphasisTextColor};
+  }
+  h2 {
+    font-size: 30px;
+    font-weight: bold;
+    text-align: center;
+  }
+  section {
+    margin-top: 10px;
+  }
+`;
+export const Qualifications = styled.ul`
+  margin-top: 20px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  ax-width: 800px;
+  gap: 20px;
+  list-style: none;
+  padding: 0;
+  margin-left: auto;
+  margin-right: auto;
+
+  li {
+    background: ${colors.tertiaryColor};
+    text-align: center;
+    padding: 10px 7px 8px 7px;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  img {
+    width: 16rem;
+    max-width: 100%;
+  }
+`;
