c96033656086c2edb62facf33012c6bd15199b6f
Giovanni Henrique <giovannihpf@gmail.com>
2025-05-03 13:54:04 -0300

    Tirando o componente de espaçamento do menu

--- src/components/MenuSpacing/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { MenuSpacingDiv } from './styled';
-
-export function MenuSpacing() {
-  return <MenuSpacingDiv></MenuSpacingDiv>;
-}
--- src/components/MenuSpacing/styled.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const MenuSpacingDiv = styled.div`
-  padding: 16rem;
-`;
